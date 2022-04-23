dcb09c187fe9c8cafebaf045e64ba0a2f447bf33
EOS5 <victor.li@esme.fr>
2022-04-23 16:22:24 +0200

    Add function to calculate Distance

--- Contraintes.js
+++ Contraintes.js
@@ -1,6 +1,24 @@
 import * as R from 'ramda'
+import {List} from "./Map.js";
 
-const CalculDistance = (x, y) => {
+const getArrayPosition = () => {
+    const listArrayPosition = [];
+    for (let i = 0; i<List.length; i++){
+        listArrayPosition[i] = List[i].getCorrdEntrepot();
+    }
+    return listArrayPosition;
+}
+
+const CalcDistance = positionCamion => {
+    var sum = 0;
+    for (let i = 0; i < getArrayPosition().length; i++){
+        var d = GetVector(positionCamion,List[i].getCorrdEntrepot());
+        sum = sum + d;
+    }
+    return sum;
+}
+
+const GetVector = (x, y) => {
     return Math.sqrt(Math.pow(R.subtract(x[0],y[0]),2) + Math.pow(R.subtract(x[1],y[1]),2));
 }
 
@@ -13,5 +31,5 @@ const random = R.curry((min, max, isFloat) => {
 })
 
 
-export {CalculDistance,CalculPaye,random}
+export {GetVector,CalculPaye,random,CalcDistance}
 
--- Map.js
+++ Map.js
@@ -1,25 +1,33 @@
-import {CalculDistance, CalculPaye, random} from "./Contraintes.js";
+import {GetVector, CalculPaye, random, CalcDistance} from "./Contraintes.js";
 
 import {Distributeur, Marchandise} from "./Ville.js";
 import {Camion} from "./Camion.js";
 
-const List = []
+
+const List = [];
 for (var i = 0;i<2;i++){
     List[i] = new Marchandise("Paris", random(1, 10, false), random(1, 10, false), "Chanel", random(1, 500, false), random(1, 5, false));
 }
-const Camion1 = new Camion(1,350,0,0,0);
+const ListOfTrucks  = [];
+for (var j = 0;j<2;j++){
+    ListOfTrucks[j] = new Camion(j,random(100,500,false),0,0,0,random(1,10,false),random(1,10,false));
+}
+const Camion1 = new Camion(1,350,0,0,0,random(1,10,false),random(1,10,false));
 const distributeur = new Distributeur("Paris",random(1,10,false),random(1,10,false),"Leclerc",random(1,500,false));
 
 
 console.log(List)
+console.log(ListOfTrucks);
 console.log(List[0].getNomEntrepot());
 console.log(distributeur.getStock());
-Camion1.addDistance(CalculDistance(distributeur.getCorrd(),List[0].getCorrdEntrepot()));
+Camion1.addDistance(GetVector(distributeur.getCorrd(),List[0].getCorrdEntrepot()));
 Camion1.addCargo(300);
 Camion1.addPaye(CalculPaye(Camion1.getCargo(),List[0].getPrixUnit()));
 console.log(Camion1.getDiscanceParcourue());
 console.log(Camion1.getPaye());
 console.log(List[1].getCorrdEntrepot());
 console.log(distributeur.getCorrd());
+console.log(Camion1.getCoordCamion());
+console.log(CalcDistance(Camion1.getCoordCamion()))
 
-
+export {List}
