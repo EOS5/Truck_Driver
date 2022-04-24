dfb22170caa0c3fc64b0d98893fe74ac9fc1fe57
EOS5 <victor.li@esme.fr>
2022-04-25 00:49:46 +0200

    Calculate Vector btw all points

--- Map.js
+++ Map.js
@@ -28,5 +28,5 @@ const createPoint = R.times(generatePoint);
 const fleet = createFleet(2);
 const wareHouses = createPoint(10);
 
-console.log(normVector(fleet[1],wareHouses[1]));
-// console.log(R.map())
+// console.log(normVector(fleet[1],wareHouses[1]));
+console.log(R.map(normVector(fleet[1]),wareHouses))
