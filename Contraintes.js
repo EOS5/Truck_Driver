f703a7f568bcc3dcaf5d96f9c3299c9a87746acf
EOS5 <victor.li@esme.fr>
2022-04-24 14:21:38 +0200

    Retry

--- Contraintes.js
+++ Contraintes.js
@@ -1,9 +1,5 @@
 import * as R from 'ramda'
 
-const Random = R.curry((min, max, isFloat) => {
-    return isFloat ? Math.random() * (max - min) + min : Math.floor(Math.random() * (max - min) + min);
-})
-
 const GetVector = (x, y) => {
     return Math.sqrt(Math.pow(R.subtract(x[0],y[0]),2) + Math.pow(R.subtract(x[1],y[1]),2));
 }
@@ -11,8 +7,6 @@ const GetVector = (x, y) => {
 const CalculPaye = (quantite, prixUnite) => {
     return R.multiply(quantite,prixUnite);
 }
-
-
 
 const shufflelist = (list,index1,index2) => {
     let temp = list[index1];
@@ -22,5 +16,5 @@ const shufflelist = (list,index1,index2) => {
 
 
 
-export {GetVector,CalculPaye,Random,shufflelist}
+export {GetVector,CalculPaye,shufflelist}
 
--- Map.js
+++ Map.js
@@ -1,50 +1,32 @@
-import {GetVector, Random,} from "./Contraintes.js";
+import * as R from 'ramda'
 
-import {Distributeur, Marchandise} from "./Ville.js";
-import {Camion} from "./Camion.js";
-
-const listPoints = [];
-const listOfTrucks  = [];
-const listDistributeur = [];
-
-for (let i = 0; i<5; i++){
-    listPoints[i] = new Marchandise("Paris", Random(1, 10, false), Random(1, 10, false), "Chanel", Random(1, 500, false), Random(1, 5, false));
+const randomNumberGenerator = R.curry((min, max, isFloat) => {
+    return isFloat ? (max - min) : Math.floor(Math.random() * (max - min) + min);
+})
+const possiblePath = (quantite) => {
+    return R.scan(R.multiply, 1, quantite);
 }
-for (let j = 0; j<2; j++){
-    listOfTrucks[j] = new Camion(j,Random(100,500,false),0,0,0,Random(1,10,false),Random(1,10,false));
+const generateNumber = () =>{
+    return R.identity(randomNumberGenerator(1,10,false));
 }
-for (let j = 0; j<2; j++) {
-    listDistributeur[j] = new Distributeur("Paris", Random(1, 10, false), Random(1, 10, false), "Leclerc", Random(1, 500, false));
+const generatePoints = (quantite) => {
+    return R.times(generateNumber, quantite);
 }
 
-const listArrayPosition = [];
-const getArrayPosition = () => {
-    for (let i = 0; i<listPoints.length; i++){
-        listArrayPosition[i] = listPoints[i].getCorrdEntrepot();
-    }
+const numberToTuple = (listOfPoints) => {
+    return R.aperture(2,listOfPoints)
 }
+const generateCoordonates = (quantite) => {
+    return numberToTuple(generatePoints(quantite));
+}
+//-----------------------------------------------------
+//const GetVector = (x, y) => {
+//    return Math.sqrt(Math.pow(R.subtract(x[0],y[0]),2) + Math.pow(R.subtract(x[1],y[1]),2));
+//}
+//const VectorfromCoordonates = (listOfCoordonates) => {
 
-const calcDistance = (positionCamion) => {
-    var sum = 0;
-    for (let i = 0; i < listArrayPosition.length; i++){
-        var d = GetVector(positionCamion,listArrayPosition[i]);
-        sum = sum + d;
-    }
-    return sum;
-}
-const getClosestPoint = (PositionCamion) => {
-    var smallestValue = calcDistance(listOfTrucks[0].getCoordCamion())
-    for (let i = 0; i < listArrayPosition.length; i++){
-        var d = GetVector(PositionCamion,listArrayPosition[i]);
-        if (smallestValue > d){
-            smallestValue = listArrayPosition[i];
-        }
-    }
-    return smallestValue;
-}
-
-console.log(listOfTrucks[0].getCoordCamion())
-getArrayPosition();
-calcDistance(listOfTrucks[0].getCoordCamion());
-listOfTrucks[0].setCoordCamion(getClosestPoint(listOfTrucks[0].getCoordCamion()));
-console.log(listOfTrucks[0].getCoordCamion())
+//}
+//-----------------------------------------------------
+const Coordonnee = generateCoordonates(10);
+console.log(Coordonnee);
+console.log(possiblePath(10))
