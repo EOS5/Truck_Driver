b6dd74b62571a24f82780c983b61c3d25cb40459
EOS5 <victor.li@esme.fr>
2022-04-24 20:17:16 +0200

    Retry

--- Map.js
+++ Map.js
@@ -6,9 +6,35 @@ const QuantitiesOfTrucks = 2;
 const Points = R.aperture(2,R.times(randomfunc, QuantitiesPoints));
 const Trucks = R.aperture(2,R.times(randomfunc, QuantitiesOfTrucks));
 
-console.log(Points)
-console.log(Trucks)
+//console.log(Points)
+//console.log(Trucks)
+
+
+//-----------------------------Fonctionne pas
+const calculDistance = (x) => {
+    Math.sqrt(Math.pow(R.subtract( Trucks[0][0], x[0]),2) + Math.pow(R.subtract( Trucks[0][1], x[1]),2));
+}
+//const f = R.map(calculDistance,Points)
+const f = R.pipe(
+    R.map(calculDistance,Points),
+    R.sort,
+)
+console.log(f)
+
+//-----------------------------------------
+const coord = R.map
+const calcVecteur = R.pipe(
+    coord,
+    calculDistance,
+)
+console.log(calcVecteur(Points))
+
+//-----------------------------------------
+const G = R.pipe(Math.pow, R.negate, R.inc);
+
+console.log(G(3, 4));
 
 
 
-//const f = R.map(console.log,Points)
+
+
