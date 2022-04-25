785e9b857c99f42cfb2bb0719cfe1fb4d49f1523
EOS5 <victor.li@esme.fr>
2022-04-25 16:43:37 +0200

    Moved warehouse and fleet generators to population

--- Map.js
+++ Map.js
@@ -1,32 +1,27 @@
 import * as R from 'ramda'
-import {randomFunc,normVector} from "./Contraintes.js";
+import {wareHouses,fleet} from "./Population.js";
 
 
-const generateTruck = R.applySpec({
-    name: "Truck1",
-    trunk: R.always(Infinity),
-    fuelTank: R.always(Infinity),
-    pos: {
-        x: randomFunc,
-        y: randomFunc
-    }
-});
-
-const createFleet = R.times(generateTruck);
-
-
-const generatePoint = R.applySpec({
-    name: 'Point1',
-    pos: {
-        x: randomFunc,
-        y: randomFunc
-    }
-});
-
-const createPoint = R.times(generatePoint);
-
-const fleet = createFleet(2);
-const wareHouses = createPoint(10);
-
+// console.log(R.map(normVector(fleet[1]),(wareHouses)))
 // console.log(normVector(fleet[1],wareHouses[1]));
-console.log(R.map(normVector(fleet[1]),wareHouses))
+
+
+const points = R.prop('pos')
+const listDistancePointsToTruck = R.applySpec({
+    pos: points,
+    distance: normVector(fleet[1])
+})
+
+const listOfPointsToTruck = R.map(listDistancePointsToTruck,wareHouses)
+const sortByDistance = R.sortBy(R.prop('distance'))
+console.log(sortByDistance(listOfPointsToTruck))
+
+const listOfPosWarehouse = R.applySpec({
+    pos: points,
+    distance: normVector(wareHouses[0])
+})
+
+// console.log(R.map(listOfPosWarehouse,wareHouses))
+// console.log(R.map(normVector(wareHouses[0]),(wareHouses)))
+const closestWarehouse = R.map(listOfPosWarehouse,wareHouses)
+console.log(sortByDistance(closestWarehouse))
