b22224f2c99808123bc12a0464ff113c49f68a27
EOS5 <victor.li@esme.fr>
2022-04-26 17:42:20 +0200

    "Cleaned" + Calcul Fitness +Normalize Fitness + Best Path

--- Map.js
+++ Map.js
@@ -1,28 +1,46 @@
-import * as R from 'ramda'
-import {wareHouses,fleet} from "./Population.js";
-import {normVector} from "./BasicCalculs.js";
+import * as R from 'ramda';
+import {wareHouses,populationShuffled} from "./Population.js";
+import {calcDistance} from "./BasicCalculs.js";
+
+//
+// const points = R.prop('pos');
+// const listDistancePointsToTruck = R.applySpec({
+//     pos: points,
+//     distance: normVector(fleet[1])
+// })
+//
+// const listOfPointsToTruck = R.map(listDistancePointsToTruck,wareHouses);
+// const sortByDistance = R.sortBy(R.prop('distance'));
+// const distanceSorted = sortByDistance(listOfPointsToTruck);
+
+//
+// const listOfPosWarehouse = R.applySpec({
+//     pos: points,
+//     distance: normVector(wareHouses[0])
+// })
+//
+// const closestWarehouse = R.map(listOfPosWarehouse,wareHouses);
+// console.log(sortByDistance(closestWarehouse));
+
+const distanceByPath = R.map(calcDistance(wareHouses),populationShuffled);
+const square = n => n * n;
+const bestDistance = R.reduce(R.minBy(square),Infinity,distanceByPath);
+const calculFitness = (x) => R.divide(1,x+1);
 
 
-// console.log(R.map(normVector(fleet[1]),(wareHouses)))
-// console.log(normVector(fleet[1],wareHouses[1]));
+const fitness = R.map(calculFitness,distanceByPath);
+const sumElemList = R.sum;
+// console.log(fitness)
+
+const normalization = R.divide(R.__,sumElemList(fitness));
+const normalizeFitness = R.map(normalization,fitness);
+
+const indexOfBestPath = R.indexOf(bestDistance,distanceByPath);
+const bestPath = populationShuffled[indexOfBestPath]
+
+console.log(fitness);
+console.log(normalizeFitness);
+console.log(bestPath);
 
 
-const points = R.prop('pos')
-const listDistancePointsToTruck = R.applySpec({
-    pos: points,
-    distance: normVector(fleet[1])
-})
 
-const listOfPointsToTruck = R.map(listDistancePointsToTruck,wareHouses)
-const sortByDistance = R.sortBy(R.prop('distance'))
-console.log(sortByDistance(listOfPointsToTruck))
-
-const listOfPosWarehouse = R.applySpec({
-    pos: points,
-    distance: normVector(wareHouses[0])
-})
-
-// console.log(R.map(listOfPosWarehouse,wareHouses))
-// console.log(R.map(normVector(wareHouses[0]),(wareHouses)))
-const closestWarehouse = R.map(listOfPosWarehouse,wareHouses)
-console.log(sortByDistance(closestWarehouse))
