06600c7037edb974c206184604a48885e26bed6f
EOS5 <victor.li@esme.fr>
2022-04-28 18:52:11 +0200

    Add swap

--- Shuffle.js
+++ Shuffle.js
@@ -14,5 +14,15 @@ const shuffler = R.curry(function(random, list) {
         return result;
     })
 
-
-export {shuffler}
+const swap = R.curry((index1, index2, list) => {
+    if (index1 < 0 || index2 < 0 || index1 > list.length - 1 || index2 > list.length - 1) {
+        return list // index out of bound
+    }
+    const value1 = list[index1]
+    const value2 = list[index2]
+    return R.pipe(
+        R.set(R.lensIndex(index1), value2),
+        R.set(R.lensIndex(index2), value1)
+    )(list)
+})
+export {shuffler,swap}
