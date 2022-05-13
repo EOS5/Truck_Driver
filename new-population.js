184ba0fd5b40f99c566c97ce5a7228cd5c9d07a2
EOS5 <victor.li@esme.fr>
2022-05-13 11:22:25 +0200

    loop in wip

--- new-population.js
+++ new-population.js
@@ -8,7 +8,21 @@ const createNextGeneration = R.map(newDistance, createNewOrder);
 const nextGeneration = R.sort(byDistance, createNextGeneration);
 
 // Ici je voulais loop les 4 lignes au dessus pour avoir le meilleur path
-
+// const jsp3 = (listOrder,listToCalcul) => {
+//     return R.applySpec({
+//         order: R.map(
+//             R.pipe(
+//                 R.prop('order'),
+//                 swap(randomNumber, randomNumber)
+//             ),(listOrder)),
+//         distance: R.map(
+//             R.pipe(
+//                 R.prop('order'),
+//                 calcDistanceWithOrder(wareHouses)
+//             ),(listToCalcul))
+//     })
+// };
+// console.log(jsp3(newPopulation,newPopulation));
 console.log('Best Path:', nextGeneration[0]);
 
 export {createNewOrder, createNextGeneration, nextGeneration};
